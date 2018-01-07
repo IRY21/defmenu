6608200e587987f86758a7f9e3796b7e05ef3547
IRY21 <rilok21work@gmail.com>
2018-01-07 14:26:56 +0500

    Update def-menu.js

--- js/def-menu.js
+++ js/def-menu.js
@@ -1,105 +1,108 @@
 
-    function DefMenu(options) {
-        if (!options) return;
+function DefMenu(options) {
+    if (!options) return;
 
-        this._state = 0;
-        var self = this;
+    this._state = 0;
+    var self = this;
 
-        this._menuClass = options.menu || false;
-        this._btnName = options.btn || false;
+    this._menuClass = options.menu || false;
+    this._btnName = options.btn || false;
 
-        this._links = options.links || false;
-        this._build = options.build || false;
+    this._links = options.links || false;
+    this._build = options.build || false;
 
-        this._btn = document.querySelector(this._btnName);
-        this._menuContainer = document.querySelector(this.MENUCONTAINERNAME);
+    this._btn = document.querySelector(this._btnName);
+    this._menuContainer = document.querySelector(this.MENUCONTAINERNAME);
 
-        if(this._build || !this._menuClass) this._createNav(this._menuClass);
-        if(this._links) this._createLinks(this._links);
+    if(this._build || !this._menuClass) this._createNav(this._menuClass);
+    if(this._links) this._createLinks(this._links);
 
-        $(this._btn).click(function () {
-            self.run();
-        });
+    $(this._btn).click(function () {
+        self.run();
+    });
 
-        $(document).on('click touchstart', function (evt) {
-            var target = evt.target;
+    $(document).on('click touchstart', function (evt) {
+        var target = evt.target;
 
-            while(target != this){
-                if (target == self._menuContainer){
-                    self.close();
-                    return;
-                }
-                if (target == self._btn){
-                    return;
-                }
-                target = target.parentNode;
+        while(target != this){
+            if (target == self._menuContainer){
+                self.close();
+                return;
             }
-        });
+            if (target == self._btn){
+                return;
+            }
+            target = target.parentNode;
+        }
+    });
+}
+
+DefMenu.prototype.MENUCONTAINERNAME = "#defmenu-container";
+DefMenu.prototype.MENUWRAPPERNAME = "#defmenu-wrapper";
+
+DefMenu.prototype._createNav = function (menuName) {
+    var menu, div, ul;
+
+    menu = document.querySelector(menuName);
+
+    if (!menu) {
+        menu = document.createElement("div");
+        menu.className = "defmenu";
+        var navParent = document.querySelector(this.MENUWRAPPERNAME) || document.body;
+        navParent.appendChild(menu);
     }
 
-    DefMenu.prototype.MENUCONTAINERNAME = "#defmenu-container";
-    DefMenu.prototype._createNav = function (menuName) {
-        var menu, div, ul;
+    menu.classList.add("defmenu");
 
-        menu = document.querySelector(menuName);
+    div = document.createElement("div");
+    div.className = "defmenu-content";
 
-        if (!menu) {
-            menu = document.createElement("div");
-            menu.className = "defmenu";
-            document.body.appendChild(menu);
-        }
+    ul = document.createElement("ul");
+    ul.className = "defmenu-nav";
 
-        menu.classList.add("defmenu");
+    div.appendChild(ul);
 
-        div = document.createElement("div");
-        div.className = "defmenu-content";
+    menu.appendChild(div);
 
-        ul = document.createElement("ul");
-        ul.className = "defmenu-nav";
+    if (!this._links) this._createLinks("auto");
+};
+DefMenu.prototype._createLinks =  function (createState) {
+    var parent = document.querySelector(".defmenu-nav");
+    var anchorBlocks = document.querySelectorAll("*[data-defanchor]");
 
-        div.appendChild(ul);
-
-        menu.appendChild(div);
-
-        if (!this._links) this._createLinks("auto");
-    };
-    DefMenu.prototype._createLinks =  function (createState) {
-        var parent = document.querySelector(".defmenu-nav");
-        var anchorBlocks = document.querySelectorAll("*[data-defanchor]");
-
-        switch (createState){
-            case "auto":
-                var anchor, text, li, a, navElems;
-                for (var i = 0; i < anchorBlocks.length; i++){
-                    anchor = anchorBlocks[i].getAttribute("id");
-                    text = anchorBlocks[i].getAttribute("data-defanchor");
-                    li = document.createElement("li");
-                    a = document.createElement("a");
-                    a.className = "defmenu-nav__link";
-                    a.href = "#" + anchor;
-                    a.innerHTML = text;
-                    li.appendChild(a);
-                    parent.appendChild(li);
-                }
-                break;
-        }
-    };
-    DefMenu.prototype.open = function () {
-        $(this._btnName).removeClass("defmenu-btn-closed");
-        $(this._btnName).addClass("defmenu-btn-open");
-        $("body").addClass("defmenu-open");
-        this._state = 1;
-    };
-    DefMenu.prototype.close =  function () {
-         $(this._btnName).removeClass("defmenu-btn-open");
-         $(this._btnName).addClass("defmenu-btn-closed");
-         $("body").removeClass("defmenu-open");
-        this._state = 0;
+    switch (createState){
+        case "auto":
+            var anchor, text, li, a, navElems;
+            for (var i = 0; i < anchorBlocks.length; i++){
+                anchor = anchorBlocks[i].getAttribute("id");
+                text = anchorBlocks[i].getAttribute("data-defanchor");
+                li = document.createElement("li");
+                a = document.createElement("a");
+                a.className = "defmenu-nav__link";
+                a.href = "#" + anchor;
+                a.innerHTML = text;
+                li.appendChild(a);
+                parent.appendChild(li);
+            }
+            break;
     }
-    DefMenu.prototype.run =  function () {
-        if(this._state == 0){
-            this.open();
-        }else{
-            this.close();
-        }
-    };
+};
+DefMenu.prototype.open = function () {
+    $(this._btnName).removeClass("defmenu-btn-closed");
+    $(this._btnName).addClass("defmenu-btn-open");
+    $("body").addClass("defmenu-open");
+    this._state = 1;
+};
+DefMenu.prototype.close =  function () {
+    $(this._btnName).removeClass("defmenu-btn-open");
+    $(this._btnName).addClass("defmenu-btn-closed");
+    $("body").removeClass("defmenu-open");
+    this._state = 0;
+};
+DefMenu.prototype.run =  function () {
+    if(this._state == 0){
+        this.open();
+    }else{
+        this.close();
+    }
+};
